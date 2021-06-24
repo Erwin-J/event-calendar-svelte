99ae0ca929f20b881dcd1708317eff5376bcd5c5
Erwin-j <erwin-j@outlook.com>
2021-06-24 21:51:11 +0200

    default grid setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
-<script lang="ts">
-	export let name: string;
+<script>
+  import Calendar from "./Calendar.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Calendar />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    font-family: Verdana, sans-serif;
+  }
 </style>
---
+++ src/Calendar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  const currentDate: Date = new Date();
+
+  const rows: number = 5;
+  const columns: number = 7;
+
+  $: col = `repeat(${columns}, 1fr)`;
+  $: row = `repeat(${rows}, 1fr)`;
+</script>
+
+<div
+  class="calendar-container"
+  style="grid-template-rows: {row}; grid-template-columns: {col};"
+>
+  {#each { length: rows } as _, i (i)}
+    {#each { length: columns } as _, j (j)}
+      <div class="calendar-cell">{i},{j}</div>
+    {/each}
+  {/each}
+</div>
+
+<style>
+  .calendar-container {
+    display: grid;
+    border: 1px solid #999;
+    border-radius: 2px;
+    width: 100%;
+    height: 100%;
+    grid-gap: 1px;
+    background: #999;
+  }
+
+  .calendar-cell {
+    border: 1px solid #444444;
+    padding: 20px;
+  }
+</style>
