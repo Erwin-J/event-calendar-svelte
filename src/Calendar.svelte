94cbf636b4971d39c6ada5f364634eafdc52c800
Erwin-j <erwin-j@outlook.com>
2021-06-26 15:33:18 +0200

    wip on the scheduler, figuring out some stuff

--- src/Calendar.svelte
+++ src/Calendar.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { writable } from "svelte/store";
   import { viewCurrentDate } from "./calendar.store";
+  import Scheduler from "./Scheduler.svelte";
   import { days } from "./utils/date.utils";
 
   const selectedDate = new Date();
+  let scheduleDay: number;
+  let isVisible: boolean;
 
   const daysInMonth = writable(
     new Date(
@@ -20,16 +23,30 @@
       1
     ).getDay();
 
-    if (firstWeekDayOfTheMonth === 0 && dayIndex === 1) {
+    if (dayStartsAtSunday(firstWeekDayOfTheMonth, dayIndex)) {
       return `grid-column-start: ${7};`;
     }
 
-    if (dayIndex === 1) {
+    if (isFirstDayOfTheMonth(dayIndex)) {
       return `grid-column-start: ${firstWeekDayOfTheMonth};`;
     }
 
     return "";
   };
+
+  function dayClickedEvent(dayIndex: number) {
+    console.log(dayIndex);
+    scheduleDay = dayIndex;
+    isVisible = true;
+  }
+
+  function isFirstDayOfTheMonth(dayIndex: number) {
+    return dayIndex === 1;
+  }
+
+  function dayStartsAtSunday(firstWeekDayOfTheMonth: number, dayIndex: number) {
+    return firstWeekDayOfTheMonth === 0 && dayIndex === 1;
+  }
 
   function nextMonth(): void {
     selectedDate.setMonth(selectedDate.getMonth() + 1);
@@ -58,35 +75,42 @@
   }
 </script>
 
-<div id="calendar-info">
-  <div class="leftpicker" on:click={() => previousMonth()}>🥕</div>
-  <div class="currentMonthYear">
-    {$viewCurrentDate}
-  </div>
-  <div class="rightpicker" on:click={() => nextMonth()}>🥒</div>
-</div>
-
-<div id="calendar-container">
-  {#each days as day}
-    <div class="day">{day}</div>
-  {/each}
-
-  {#each { length: $daysInMonth } as _, dayIndex}
-    <div
-      class="day"
-      style={$daysInMonth ? startingDayPosition(dayIndex + 1) : ""}
-    >
-      {dayIndex + 1}
+<div id="calendar-wrapper">
+  <div id="calendar-info">
+    <div class="leftpicker" on:click={() => previousMonth()}>🥕</div>
+    <div class="currentMonthYear">
+      {$viewCurrentDate}
     </div>
-  {/each}
+    <div class="rightpicker" on:click={() => nextMonth()}>🥒</div>
+  </div>
+
+  <div id="calendar-container">
+    {#each days as day}
+      <div class="day">{day}</div>
+    {/each}
+
+    {#each { length: $daysInMonth } as _, dayIndex}
+      <div
+        class="day-cell"
+        style={$daysInMonth ? startingDayPosition(dayIndex + 1) : ""}
+        on:click={() => dayClickedEvent(dayIndex + 1)}
+      >
+        {dayIndex + 1}
+      </div>
+    {/each}
+  </div>
 </div>
+<Scheduler day={scheduleDay} schedulerVisible={isVisible} />
 
 <style>
+  #calendar-wrapper {
+    margin: 8px;
+  }
   #calendar-container {
     display: grid;
     grid-template-columns: repeat(7, 1fr);
 
-    background: #eaeaea;
+    background: #efefef;
     height: 100%;
   }
   #calendar-info {
@@ -104,17 +128,24 @@
   .leftpicker {
     justify-self: start;
   }
-  .leftpicker:hover {
-    cursor: pointer;
-  }
   .rightpicker {
     justify-self: end;
   }
+  .leftpicker:hover,
   .rightpicker:hover {
     cursor: pointer;
+  }
+  .day-cell {
+    padding: 20px;
+    border: 1px solid #dbdbdb;
+    background: #eaeaea;
+  }
+  .day-cell:hover {
+    background: #b1b1b1;
   }
   .day {
     padding: 20px;
     border: 1px solid #dbdbdb;
+    background: #ffffff;
   }
 </style>
---
+++ src/Scheduler.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  export let schedulerVisible: boolean;
+  export let day: number;
+
+  function handleClickOutside() {
+    schedulerVisible = false;
+  }
+</script>
+
+{#if schedulerVisible}
+  <div id="calendar-scheduler" in:fade out:fade>
+    <h1>Create new event for day {day}</h1>
+    <form>
+      <label for="eventName">Event Name:</label>
+      <input type="text" id="eventName" name="eventName" />
+      <br /><br />
+      <label for="timeName">Time:</label>
+      <input
+        type="time"
+        id="appt"
+        name="timeName"
+        min="09:00"
+        max="18:00"
+        required
+      />
+      <input
+        type="time"
+        id="appt"
+        name="timeName"
+        min="09:00"
+        max="18:00"
+        required
+      />
+      <br /><br />
+      <input type="submit" name="submit" value="Create Event" />
+    </form>
+  </div>
+  <div
+    class="scheduler-filler"
+    in:fade
+    out:fade
+    on:click={() => (schedulerVisible = false)}
+  />
+{/if}
+
+<style>
+  h1 {
+    text-align: center;
+  }
+  #calendar-scheduler {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    background: #ffffffe6;
+    height: 300px;
+    border-bottom: 5px solid rgba(99, 108, 238, 0.5);
+    text-align: center;
+    z-index: 100;
+  }
+  .scheduler-filler {
+    position: absolute;
+    bottom: 0;
+    background: #ffffffe2;
+    height: 100%;
+    width: 100%;
+    z-index: 95;
+  }
+</style>
